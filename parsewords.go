dae8e6171fa8fd5d5a0f5117b7ccae21ec4eea26
Daniel Podolsky <onokonem@gmail.com>
2017-07-26 13:35:17 +0300

    half of work

--- parsewords.go
+++ parsewords.go
@@ -1,1 +1,332 @@
 package parsewords
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type keepType int
+
+// Possible keep argument values
+//
+// The keep argument is a semi-boolean flag.  If not KeepNothing, then the tokens are
+// split on the specified delimiter, but all other characters (including
+// quotes and backslashes) are kept in the tokens.  If keep is KeepNothing then the
+// Quotewords() functions remove all quotes and backslashes that are
+// not themselves backslash-escaped or inside of single quotes (i.e.,
+// Quotewords() tries to interpret these characters just like the Bourne
+// shell).
+//
+// As an additional feature, keep may be KeepDelimiters value which
+// causes the functions to preserve the delimiters in each string as
+// tokens in the token lists, in addition to preserving quote and
+// backslash characters.
+const (
+	KeepNothing = iota
+	KeepQuotes
+	KeepDelimiters
+)
+
+var (
+	unslashRegexp  = regexp.MustCompile(`(?s:\\(.))`)
+	unslashReplace = `$1`
+)
+
+// Shellwords is written as a special case of &quotewords(), and it
+// does token parsing with whitespace as a delimiter-- similar to most
+// Unix shells.
+func Shellwords(lines ...string) ([]string, error) {
+	allwords := make([]string, 0, len(lines))
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		words, err := ParseLine(`\s+`, KeepNothing, line)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(words) > 0 && words[len(words)-1] == "" {
+			words = words[:len(words)-1]
+		}
+
+		if !(len(words) > 0 || len(line) == 0) {
+			return nil, nil
+		}
+		allwords = append(allwords, words...)
+	}
+
+	return allwords, nil
+}
+
+// ParseLine does tokenizing on a single string.
+func ParseLine(delimiter string, keep keepType, line string) ([]string, error) {
+	delimiting, err := regexp.Compile(delimiter)
+	if err != nil {
+		return nil, err
+	}
+
+	words, err := smartSplit(delimiting, line)
+	if len(words) == 0 || err != nil {
+		return nil, err
+	}
+
+	pieces := make([]string, 0, len(words))
+	for _, word := range words {
+		piece := ""
+
+		for _, token := range word.tokens {
+			str := line[token.on:token.off]
+			if keep == KeepNothing {
+				str = unquote(str)
+			}
+			piece += str
+		}
+
+		pieces = append(pieces, piece)
+
+		if keep == KeepDelimiters && word.delimiter.on > 0 {
+			pieces = append(pieces, line[word.delimiter.on:word.delimiter.off])
+		}
+	}
+
+	return pieces, nil
+}
+
+func unquote(str string) string {
+	switch {
+	case len(str) == 0:
+		return ""
+	case str[0] == '\'':
+		if len(str) > 2 {
+			return str[1 : len(str)-1]
+		}
+	case str[0] == '"':
+		if len(str) > 2 {
+			return unslashRegexp.ReplaceAllString(str[1:len(str)-1], unslashReplace)
+		}
+	}
+
+	return unslashRegexp.ReplaceAllString(str, unslashReplace)
+}
+
+// Quotewords and NestedQuotewords functions accept a delimiter
+// (which can be a regular expression)
+// and a list of lines and then breaks those lines up into a list of
+// words ignoring delimiters that appear inside quotes.  Quotewords()
+// returns all of the tokens in a single long list, while NestedQuotewords()
+// returns a list of token lists corresponding to the elements of lines[].
+func Quotewords(delimiter string, keep keepType, lines ...string) ([]string, error) {
+	allwords := make([]string, 0, len(lines))
+
+	for _, line := range lines {
+		words, err := ParseLine(delimiter, keep, line)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(words) > 0 && words[len(words)-1] == "" {
+			words = words[:len(words)-1]
+		}
+
+		if !(len(words) > 0 || len(line) == 0) {
+			return nil, nil
+		}
+		allwords = append(allwords, words...)
+	}
+
+	return allwords, nil
+}
+
+// NestedQuotewords and Quotewords functions accept a delimiter
+// (which can be a regular expression)
+// and a list of lines and then breaks those lines up into a list of
+// words ignoring delimiters that appear inside quotes.  Quotewords()
+// returns all of the tokens in a single long list, while NestedQuotewords()
+// returns a list of token lists corresponding to the elements of lines[].
+func NestedQuotewords(delimiter string, keep keepType, lines ...string) ([][]string, error) {
+	allwords := make([][]string, 0, len(lines))
+
+	for _, line := range lines {
+		words, err := ParseLine(delimiter, keep, line)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(words) > 0 && words[len(words)-1] == "" {
+			words = words[:len(words)-1]
+		}
+
+		if !(len(words) > 0 || len(line) == 0) {
+			return nil, nil
+		}
+		allwords = append(allwords, words)
+	}
+
+	return allwords, nil
+}
+
+type substring struct {
+	on  int
+	off int
+}
+
+type delimitedWord struct {
+	tokens    []substring
+	delimiter substring
+}
+
+func delimitedWordNew(token substring, delimiter substring) delimitedWord {
+	return delimitedWord{
+		[]substring{token},
+		badSubstring,
+	}
+}
+
+func smartSplit(delimiting *regexp.Regexp, line string) ([]delimitedWord, error) {
+	quoted, slashed, err := enumerateQuotes(line)
+	if err != nil {
+		return nil, err
+	}
+
+	delimiters := enumerateDelimiters(line, quoted, slashed, delimiting)
+
+	if len(delimiters) == 0 {
+		return []delimitedWord{delimitedWordNew(substring{0, len(line)}, badSubstring)}, nil
+	}
+
+	return enumerateWords(line, delimiters, quoted), nil
+}
+
+func enumerateQuotes(line string) ([]substring, []int, error) {
+	quoted := make([]substring, 0, 10)
+	slashed := make([]int, 0, 10)
+
+	sqOn := -1
+	dqOn := -1
+	slashOn := false
+
+	for ri, sym := range line {
+		if slashOn && sqOn < 0 {
+			slashed = append(slashed, ri)
+		}
+		switch {
+		case sym == '\\':
+			slashOn = !slashOn
+		case sym == '"' && dqOn < 0 && sqOn < 0 && !slashOn:
+			dqOn = ri
+		case sym == '"' && dqOn >= 0 && !slashOn:
+			quoted = append(quoted, substring{dqOn, ri + 1})
+			dqOn = -1
+		case sym == '\'' && dqOn < 0 && sqOn < 0 && !slashOn:
+			sqOn = ri
+			slashOn = false
+		case sym == '\'' && sqOn >= 0:
+			quoted = append(quoted, substring{sqOn, ri + 1})
+			sqOn = -1
+			slashOn = false
+		default:
+			slashOn = false
+		}
+	}
+
+	if sqOn >= 0 {
+		return nil, nil, fmt.Errorf("Single quote unclosed: %d", sqOn)
+	}
+	if dqOn >= 0 {
+		return nil, nil, fmt.Errorf("Double quote unclosed: %d", dqOn)
+	}
+
+	return quoted, slashed, nil
+}
+
+func enumerateWords(
+	line string,
+	delimiters []substring,
+	quoted []substring,
+) []delimitedWord {
+	words := make([]delimitedWord, 0, len(delimiters))
+	curPos := 0
+	curQuoted := 0
+	for _, delimiter := range delimiters {
+		word := delimitedWord{make([]substring, 0, 1), badSubstring}
+
+		for curPos < delimiter.on {
+			if curQuoted < len(quoted) {
+				if curPos == quoted[curQuoted].on {
+					word.tokens = append(word.tokens, quoted[curQuoted])
+					curPos = quoted[curQuoted].off
+					curQuoted++
+					continue
+				}
+
+				if delimiter.on > quoted[curQuoted].on {
+					word.tokens = append(word.tokens, substring{curPos, quoted[curQuoted].on})
+					curPos = quoted[curQuoted].on
+					continue
+				}
+			}
+
+			word.tokens = append(word.tokens, substring{curPos, delimiter.on})
+			curPos = delimiter.off
+		}
+
+		curPos = delimiter.off
+		word.delimiter = delimiter
+		words = append(words, word)
+	}
+
+	if curPos < len(line) {
+		words = append(words, delimitedWordNew(substring{curPos, len(line)}, badSubstring))
+	}
+
+	return words
+
+}
+
+func enumerateDelimiters(
+	line string,
+	quoted []substring,
+	slashed []int,
+	delimiting *regexp.Regexp,
+) []substring {
+	matches := delimiting.FindAllStringIndex(line, -1)
+	delimiters := make([]substring, 0, len(matches))
+
+	for _, match := range matches {
+		delimeter := checkDelimiter(line, quoted, slashed, delimiting, substring{match[0], match[1]})
+		if delimeter.on > 0 {
+			delimiters = append(delimiters, delimeter)
+		}
+	}
+
+	return delimiters
+}
+
+var badSubstring = substring{-1, -1}
+
+func checkDelimiter(
+	line string,
+	quoted []substring,
+	slashed []int,
+	delimiting *regexp.Regexp,
+	delimiter substring,
+) substring {
+	for _, quote := range quoted {
+		if delimiter.on >= quote.on && delimiter.on < quote.off {
+			return badSubstring
+		}
+	}
+
+	for _, slash := range slashed {
+		if delimiter.on == slash {
+			delimiter.on++
+			if delimiting.MatchString(line[delimiter.on:delimiter.off]) {
+				return delimiter
+			}
+			return badSubstring
+		}
+	}
+
+	return delimiter
+}
--- parsewords_test.go
+++ parsewords_test.go
@@ -1,1 +1,223 @@
 package parsewords_test
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/Djarvur/parsewords"
+)
+
+func getCaller(stackBack int) string {
+	_, file, line, ok := runtime.Caller(stackBack + 1)
+	if !ok {
+		return "UNKNOWN"
+	}
+
+	if li := strings.LastIndex(file, "/"); li > 0 {
+		file = file[li+1:]
+	}
+
+	return fmt.Sprintf("%s:%d", file, line)
+}
+
+func is(t *testing.T, got string, expected string) {
+	if got != expected {
+		t.Errorf("%s: Expected %q, got %q", getCaller(1), expected, got)
+	}
+}
+
+func isInt(t *testing.T, got int, expected int) {
+	if got != expected {
+		t.Errorf("%s: Expected %v, got %v", getCaller(1), expected, got)
+	}
+}
+
+func qq(words ...string) string {
+	return strings.Join(words, " ")
+}
+
+func TestShellwords(t *testing.T) {
+	words, err := parsewords.Shellwords(qq(`foo`, `"bar quiz"`, `zoo`))
+	if err != nil {
+		t.Fatalf("Unexpected eror: %v", err)
+	}
+
+	is(t, words[0], `foo`)
+	is(t, words[1], `bar quiz`)
+	is(t, words[2], `zoo`)
+}
+
+func TestQuotewords(t *testing.T) {
+	// Test quotewords() with other parameters and null last field
+	words, err := parsewords.Quotewords(`:+`, parsewords.KeepQuotes, `foo:::"bar:foo":zoo zoo:`)
+	if err != nil {
+		t.Fatalf("Unexpected eror: %v", err)
+	}
+
+	is(t, strings.Join(words, ";"), `foo;"bar:foo";zoo zoo`)
+}
+
+func TestQuotewordsDelimiters(t *testing.T) {
+	// Test $keep eq 'delimiters' and last field zero
+	words, err := parsewords.Quotewords(`\s+`, parsewords.KeepDelimiters, `4 3 2 1 0`)
+	if err != nil {
+		t.Fatalf("Unexpected eror: %v", err)
+	}
+
+	is(t, strings.Join(words, ";"), qq(`4;`, `;3;`, `;2;`, `;1;`, `;0`))
+}
+
+// Big ol' nasty test (thanks, Joerk!)
+var BigOlNastyTest = `aaaa"bbbbb" cc\ cc \\\"dddd" eee\\\"ffff" "gg"`
+
+func TestParseLineEscapedKeepQuotes(t *testing.T) {
+	str := BigOlNastyTest
+
+	// First with $keep == 1
+	words, err := parsewords.ParseLine(`\s+`, parsewords.KeepQuotes, str)
+	if err != nil {
+		t.Fatalf("Unexpected eror: %v", err)
+	}
+
+	is(t, strings.Join(words, `|`), `aaaa"bbbbb"|cc\ cc|\\\"dddd" eee\\\"ffff"|"gg"`)
+}
+
+func TestParseLineEscapedKeepNothing(t *testing.T) {
+	str := BigOlNastyTest
+
+	// Now, $keep == 0
+	words, err := parsewords.ParseLine(`\s+`, parsewords.KeepNothing, str)
+	if err != nil {
+		t.Fatalf("Unexpected eror: %v", err)
+	}
+
+	is(t, strings.Join(words, `|`), `aaaabbbbb|cc cc|\"dddd eee\"ffff|gg`)
+}
+
+func TestParseLineSinglequote(t *testing.T) {
+	// Now test single quote behavior
+	// Note: original test behavior is unclear for me so this one is modified to make sense.
+	str := `aaaa"bbbbb" cc\ cc \\\"dddd\\' eee\\\"ffff\' gg`
+
+	words, err := parsewords.ParseLine(`\s+`, parsewords.KeepNothing, str)
+	if err != nil {
+		t.Fatalf("Unexpected eror: %v", err)
+	}
+
+	is(t, strings.Join(words, `|`), `aaaabbbbb|cc cc|\"dddd\ eee\\\"ffff\|gg`)
+}
+
+func TestNestedQuotewords(t *testing.T) {
+	// Make sure @nested_quotewords does the right thing
+	words, err := parsewords.NestedQuotewords(`\s+`, parsewords.KeepNothing, `a b c`, `1 2 3`, `x y z`)
+	if err != nil {
+		t.Fatalf("Unexpected eror: %v", err)
+	}
+
+	isInt(t, len(words), 3)
+	isInt(t, len(words[0]), 3)
+	isInt(t, len(words[1]), 3)
+	isInt(t, len(words[2]), 3)
+}
+
+func TestShellwordsError(t *testing.T) {
+	// Now test error return
+	str := `foo bar baz"bach blech boop`
+
+	words, err := parsewords.Shellwords(str)
+	if err == nil {
+		t.Errorf("Error expected but not received")
+	}
+	if len(words) > 0 {
+		t.Errorf("Empty result expected but %d elements received", len(words))
+	}
+
+	words, err = parsewords.ParseLine(`s+`, parsewords.KeepNothing, str)
+	if err == nil {
+		t.Errorf("Error expected but not received")
+	}
+	if len(words) > 0 {
+		t.Errorf("Empty result expected but %d elements received", len(words))
+	}
+
+	words, err = parsewords.Quotewords(`s+`, parsewords.KeepNothing, str)
+	if err == nil {
+		t.Errorf("Error expected but not received")
+	}
+	if len(words) > 0 {
+		t.Errorf("Empty result expected but %d elements received", len(words))
+	}
+}
+
+// @words = parse_line('s+', 0, $string);
+// is(@words, 0);
+//
+// @words = quotewords('s+', 0, $string);
+// is(@words, 0);
+//
+// {
+//   # Gonna get some more undefined things back
+//   no warnings 'uninitialized' ;
+//
+//   @words = nested_quotewords('s+', 0, $string);
+//   is(@words, 0);
+//
+//   # Now test empty fields
+//   $result = join('|', parse_line(':', 0, 'foo::0:"":::'));
+//   is($result, 'foo||0||||');
+//
+//   # Test for 0 in quotes without $keep
+//   $result = join('|', parse_line(':', 0, ':"0":'));
+//   is($result, '|0|');
+//
+//   # Test for \001 in quoted string
+//   $result = join('|', parse_line(':', 0, ':"' . "\001" . '":'));
+//   is($result, "|\1|");
+//
+// }
+//
+// # Now test perlish single quote behavior
+// $Text::ParseWords::PERL_SINGLE_QUOTE = 1;
+// $string = 'aaaa"bbbbb" cc\ cc \\\\\"dddd\' eee\\\\\"\\\'ffff\' gg';
+// $result = join('|', parse_line('\s+', 0, $string));
+// is($result, 'aaaabbbbb|cc cc|\"dddd eee\\\\"\'ffff|gg');
+//
+// # test whitespace in the delimiters
+// @words = quotewords(' ', 1, '4 3 2 1 0');
+// is(join(";", @words), qq(4;3;2;1;0));
+//
+// # [perl #30442] Text::ParseWords does not handle backslashed newline inside quoted text
+// $string = qq{"field1"	"field2\\\nstill field2"	"field3"};
+//
+// $result = join('|', parse_line("\t", 1, $string));
+// is($result, qq{"field1"|"field2\\\nstill field2"|"field3"});
+//
+// $result = join('|', parse_line("\t", 0, $string));
+// is($result, "field1|field2\nstill field2|field3");
+//
+// SKIP: { # unicode
+//   skip "No unicode",1 if $]<5.008;
+//   $string = qq{"field1"\x{1234}"field2\\\x{1234}still field2"\x{1234}"field3"};
+//   $result = join('|', parse_line("\x{1234}", 0, $string));
+//   is($result, "field1|field2\x{1234}still field2|field3",'Unicode');
+// }
+//
+// # missing quote after matching regex used to hang after change #22997
+// "1234" =~ /(1)(2)(3)(4)/;
+// $string = qq{"missing quote};
+// $result = join('|', shellwords($string));
+// is($result, "");
+//
+// # make sure shellwords strips out leading whitespace and trailng undefs
+// # from parse_line, so it's behavior is more like /bin/sh
+// $result = join('|', shellwords(" aa \\  \\ bb ", " \\  ", "cc dd ee\\ "));
+// is($result, "aa| | bb| |cc|dd|ee ");
+//
+// $SIG{ALRM} = sub {die "Timeout!"};
+// alarm(3);
+// @words = Text::ParseWords::old_shellwords("foo\\");
+// is(@words, 1);
+// alarm(0);
+//
